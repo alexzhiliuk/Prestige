bc6ab5f2aec8b9d50cad1564427ad5e95fca373f
alexandrzhyliuk <alexzhyliuk3@gmail.com>
2024-08-03 17:17:26 +0300

    calculator

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 // import * as flsFunctions from './modules/functions.js';
 import * as header from './modules/header.js';
 import * as counter from './modules/counter.js';
+import * as phoneMask from './modules/phoneMask.js';
 
 
 // flsFunctions.isWebp()
