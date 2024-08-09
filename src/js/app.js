8c7234947b05ec03b5cf6462469d6f48ab77c438
alexandrzhyliuk <alexzhyliuk3@gmail.com>
2024-08-09 14:43:34 +0300

    popup

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@ import * as header from './modules/header.js';
 import * as counter from './modules/counter.js';
 import * as phoneMask from './modules/phoneMask.js';
 import * as cases from './modules/cases.js';
+import * as popup from './modules/popup.js';
 
 
 // flsFunctions.isWebp()
