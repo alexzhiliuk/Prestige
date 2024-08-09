c9ff91afec97c91b1f4bdfda309d9c27a80d7089
alexandrzhyliuk <alexzhyliuk3@gmail.com>
2024-08-09 17:21:53 +0300

    calculator

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 // import * as flsFunctions from './modules/functions.js';
 import * as header from './modules/header.js';
 import * as counter from './modules/counter.js';
+import * as calculator from './modules/calculator.js';
 import * as phoneMask from './modules/phoneMask.js';
 import * as cases from './modules/cases.js';
 import * as popup from './modules/popup.js';
--- src/js/modules/counter.js
+++ src/js/modules/counter.js
@@ -1,35 +1,73 @@
-$(".counter__input").on("input", function() {
-    if (Number($(this).val()) < Number($(this).attr("min"))) {
-        $(this).val($(this).attr("min"))
-        $(this).parents(".counter").find(".counter__minus").addClass("disabled")
-    } else {
-        $(this).parents(".counter").find(".counter__minus").removeClass("disabled")
+function checkControls(el) {
+    $(el).parents(".counter").find(".counter__minus").removeClass("disabled")
+    $(el).parents(".counter").find(".counter__plus").removeClass("disabled")
+
+    if (Number($(el).val()) < Number($(el).attr("min"))) {
+        $(el).val($(el).attr("min"))
+        $(el).parents(".counter").find(".counter__minus").addClass("disabled")
+    } 
+    if (Number($(el).val()) == Number($(el).attr("min"))) {
+        $(el).parents(".counter").find(".counter__minus").addClass("disabled")
+    } 
+
+    if (Number($(el).val()) > Number($(el).attr("max"))) {
+        $(el).val($(el).attr("max"))
+        $(el).parents(".counter").find(".counter__plus").addClass("disabled")
+    }
+    if (Number($(el).val()) == Number($(el).attr("max"))) {
+        $(el).parents(".counter").find(".counter__plus").addClass("disabled")
+    }
+}
+
+function markAsLastClick(el) {
+    $(".counter__input").removeAttr("data-last-click")
+    $(el).attr("data-last-click", "")
+
+}
+
+function checkBind(el) {
+    
+    if (!$(el).attr('data-bind-child') && !$(el).attr('data-bind-parent')) { return }
+
+    let mainEl
+    $(el).attr('data-bind-child') ? mainEl = $(el) : mainEl = $(`#${$(el).attr('data-bind-parent')}`)
+    let bindEl = $(`#${mainEl.attr('data-bind-child')}`)
+
+    if (bindEl.val() > mainEl.val()) {
+        typeof mainEl.attr('data-last-click') !== 'undefined' ? bindEl.val(mainEl.val()) : mainEl.val(bindEl.val())
     }
     
-    if (Number($(this).val()) > Number($(this).attr("max"))) {
-        $(this).val($(this).attr("max"))
-        $(this).parents(".counter").find(".counter__plus").addClass("disabled")
-    } else {
-        $(this).parents(".counter").find(".counter__plus").removeClass("disabled")
-    }
+    checkControls(mainEl[0])
+    checkControls(bindEl[0])
+}
 
+$(".counter__input").on("input", function() {
+    markAsLastClick(this)
+    checkControls(this)
+    checkBind(this)
+})
+$(".counter__input").each(function(i, el) {
+    checkControls(el)
+    checkBind(el)
 })
 
 $(".counter__minus").on("click", function() {
     if ($(this).hasClass("disabled")) { return }
     let input = $(this).parents(".counter").find(".counter__input"),
         val = Number(input.val()),
-        min = Number(input.attr("min")),
-        max = Number(input.attr("max"))
+        min = Number(input.attr("min"))
     
     input.val(val - 1)
-    $(".counter__plus").removeClass("disabled")
+    $(this).parents(".counter").find(".counter__plus").removeClass("disabled")
+
+    markAsLastClick($(this).parents(".counter").find(".counter__input"))
+    checkBind($(this).parents(".counter").find(".counter__input"))
+
 
     if (val - 2 < min) { 
         $(this).addClass("disabled")
         return 
     }
-
 
 })
 
@@ -37,16 +75,16 @@ $(".counter__plus").on("click", function() {
     if ($(this).hasClass("disabled")) { return }
     let input = $(this).parents(".counter").find(".counter__input"),
         val = Number(input.val()),
-        min = Number(input.attr("min")),
         max = Number(input.attr("max"))
     
     input.val(val + 1)
-    $(".counter__minus").removeClass("disabled")
+    $(this).parents(".counter").find(".counter__minus").removeClass("disabled")
+
+    markAsLastClick($(this).parents(".counter").find(".counter__input"))
+    checkBind($(this).parents(".counter").find(".counter__input"))
 
     if (val + 2 > max) { 
         $(this).addClass("disabled")
         return 
     }
-
-
 })
